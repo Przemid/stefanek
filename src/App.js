3f781a0bfcd6efa7286482d953218bdce8ab5edc
Przemysław Ida <przemyslaw.ida@gmail.com>
2021-10-03 11:51:20 +0200

    pwa2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={RouteChange}>STEEVE</button>
+        <button onClick={RouteChange}>STEeEVE</button>
       </header>
     </div>
   );
